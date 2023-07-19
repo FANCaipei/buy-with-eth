c9178893e6eaad021742bdaa7a8653fc816b89ec
caipei <fancaipei@gmail.com>
2023-07-19 10:35:38 +0800

    portal web page as demo

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
+
+  const cMetamask = useCallback(() => {
+    (window as any).walletManager.connectWallet((window as any).ethereum);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +16,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type='primary' onClick={cMetamask}>connect metamask</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
