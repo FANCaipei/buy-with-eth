d2d11203ccd93dedd3a5ccec2201955332e5f178
caipei <fancaipei@gmail.com>
2023-07-19 11:31:59 +0800

    sdk: send transfer

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -6,8 +6,11 @@ import { Button } from 'antd';
 function App() {
 
   const cMetamask = useCallback(() => {
-    (window as any).walletManager.connectWallet((window as any).ethereum);
-  }, [])
+    (window as any).walletManager.connectWallet((window as any).ethereum, 'metamask');
+  }, []);
+  const requestTransfer = useCallback(() => {
+    (window as any).walletManager.requestTransfer(0.01, '0x6978De6532Cd2C94D47430C22B1bCddb53fB23aa', '0xA02bB13E8d360E9E3A1c07C3043BE140C2D8DA59');
+  }, []);
 
   return (
     <div className="App">
@@ -17,6 +20,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Button type='primary' onClick={cMetamask}>connect metamask</Button>
+        <Button type='primary' onClick={requestTransfer}>request transfer</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
--- sdk/src/WalletManager.ts
+++ sdk/src/WalletManager.ts
@@ -1,7 +1,8 @@
+import { BigNumber, ethers } from "ethers";
 import EthereumProvider from "./EthereumProvider";
 
 class WalletManager {
-    static async connectWallet(provider: any): Promise<string> {
+    static async connectWallet(provider: any, providerType: 'metamask' | 'coinbase'): Promise<string> {
         if(!provider){
             return Promise.reject('no provider');
         }
@@ -11,26 +12,28 @@ class WalletManager {
         });
         const address = account?.[0];
         if(address){
+            localStorage.setItem('buywithcrypto-provider-type', providerType);
             return Promise.resolve(address);
         }
         else{
             return Promise.reject('get account failed');
         }
     }
-    static async requestTransfer(value: string): Promise<any> {
+    static async requestTransfer(value: number /**in eth */, fromAddr: string, toAddr: string): Promise<any> {
         const currentProvider = EthereumProvider.getCurrentConnectedProvider();
         if(!currentProvider){
             return Promise.reject('no selected provider')
         }
+        const valueInWei = ethers.utils.parseEther(`${value}`).toHexString();
+        console.log('send value: ', valueInWei);
         const params = [
             {
-            //   from: '0xb60e8dd61c5d32be8058bb8eb970870f07233155',
-              to: '0xd46e8dd67c5d32be8058bb8eb970870f07244567',
-              gas: '0x76c0', // 30400
-              gasPrice: '0x9184e72a000', // 10000000000000
-              value: '0x9184e72a', // 2441406250
-              data:
-                '0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675',
+              from: fromAddr,
+              to: toAddr,
+            //   gas: '0x76c0', // 30400
+            //   gasPrice: '0x9184e72a000', // 10000000000000
+              value: valueInWei,
+              data: null,
             },
         ];
         const tx = await currentProvider.request({
@@ -38,6 +41,7 @@ class WalletManager {
             params,
         });
         console.log(tx);
+        // TODO: save transaction to server
     }
 }
 
