17e146b3b4efd8cb5e79316aa299d94910f4c9da
caipei <fancaipei@gmail.com>
2023-08-03 15:30:25 +0800

    console-ui: firebase init & auth logic

---
+++ console-ui/src/common/firebase/FirebaseManager.ts
@@ -0,0 +1,70 @@
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { Firestore, getFirestore } from "firebase/firestore";
+import {
+    Auth,
+    User,
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
+
+class FirebaseManager {
+    static app: FirebaseApp;
+    static analytics: Analytics;
+    static firestore: Firestore;
+    static auth: Auth;
+    static init() {
+        if (!FirebaseManager.app) {
+            // Your web app's Firebase configuration
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+            const firebaseConfig = {
+                apiKey: "AIzaSyBK20GGqYjmSmhRjLD77deBGo6qIKuic84",
+                authDomain: "paywithcrypto-9283c.firebaseapp.com",
+                projectId: "paywithcrypto-9283c",
+                storageBucket: "paywithcrypto-9283c.appspot.com",
+                messagingSenderId: "1698450909",
+                appId: "1:1698450909:web:e866fd6cb187042236ba34",
+                measurementId: "G-WR3KECNVMK",
+            };
+
+            // Initialize Firebase
+            FirebaseManager.app = initializeApp(firebaseConfig);
+            FirebaseManager.analytics = getAnalytics(FirebaseManager.app);
+            FirebaseManager.firestore = getFirestore(FirebaseManager.app);
+            FirebaseManager.auth = getAuth(FirebaseManager.app);
+
+            onAuthStateChanged(FirebaseManager.auth, user => {
+                if (user) {
+                    // User is signed in, see docs for a list of available properties
+                    // https://firebase.google.com/docs/reference/js/auth.user
+                    if (!user.emailVerified) {
+                        // TODO: navigate to send verify email page
+                    }
+                    // ...
+                } else {
+                    // User is signed out
+                    // TODO: navigate to auth page
+                }
+            });
+        }
+    }
+    static async login(email: string, password: string): Promise<User> {
+        if (!FirebaseManager.auth) {
+            return Promise.reject("firebase manager not be init");
+        }
+        const userCredential = await signInWithEmailAndPassword(FirebaseManager.auth, email, password);
+        return userCredential.user;
+    }
+    static async signIn(email: string, password: string): Promise<User> {
+        if (!FirebaseManager.auth) {
+            return Promise.reject("firebase manager not be init");
+        }
+        const userCredential = await createUserWithEmailAndPassword(FirebaseManager.auth, email, password);
+        return userCredential.user;
+    }
+}
+
+export default FirebaseManager;
--- console-ui/src/views/Auth.tsx
+++ console-ui/src/views/Auth.tsx
@@ -1,18 +1,62 @@
-import { Form, Input } from "antd";
+import { Button, Form, Input } from "antd";
+import { useCallback, useState } from "react";
 import { styled } from "styled-components";
 
 const AuthPage = () => {
+    const [formInstace] = Form.useForm();
+    const email = Form.useWatch("email", formInstace);
+    const password = Form.useWatch("password", formInstace);
+    const repeatPwd = Form.useWatch("repeat-password", formInstace);
+
+    const repeatPwdValidator = useCallback(
+        (_: any, value: any) => {
+            if (value !== password) {
+                return Promise.reject("Repeat password is not the same as password");
+            }
+            return Promise.resolve();
+        },
+        [password]
+    );
+
+    const signIn = useCallback(async () => {
+        const validateResult = await formInstace.validateFields();
+        console.log("validate result: ", validateResult);
+        if (password !== repeatPwd) {
+            return;
+        }
+        // TODO: call firebase signin
+        console.log(email, password, repeatPwd);
+    }, [password, repeatPwd, email, formInstace]);
+
     return (
         <StyledContainer>
             <div className="title">Title</div>
-            <Form>
-                <Form.Item name="email">
+            <Form form={formInstace}>
+                <Form.Item
+                    name="email"
+                    rules={[
+                        { required: true, message: "Email is required" },
+                        { pattern: /^[\w-.]+@([\w-]+.)+[\w-]+$/g, message: "Invalid email format" },
+                    ]}
+                >
                     <Input type="email" placeholder="Email" />
                 </Form.Item>
-                <Form.Item name="password">
+                <Form.Item name="password" rules={[{ required: true, message: "Password is required" }]}>
                     <Input type="password" placeholder="Password" />
                 </Form.Item>
+                <Form.Item
+                    name="repeat-password"
+                    rules={[
+                        { required: true, message: "Repeat password is required" },
+                        { validator: repeatPwdValidator },
+                    ]}
+                >
+                    <Input type="repeat-password" placeholder="Repeat your password" />
+                </Form.Item>
             </Form>
+            <Button type="primary" onClick={signIn}>
+                SignIn
+            </Button>
         </StyledContainer>
     );
 };
