18211e0e5c817801ee4a8ab50d2a976b5b575a78
caipei <fancaipei@gmail.com>
2023-10-19 17:10:41 +0800

    console: email verify page

--- console-ui/src/views/EmailVerify.tsx
+++ console-ui/src/views/EmailVerify.tsx
@@ -1,22 +1,29 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { styled } from "styled-components";
 import FirebaseManager from "../common/firebase/FirebaseManager";
 import { Button, message } from "antd";
 import { useNavigate } from "react-router-dom";
+import { ArrowRightOutlined } from "@ant-design/icons";
 import useProtectedPath from "../common/hooks/useProtectedPath";
+import OcelotLogo from "../assets/imgs/logos/logo.png";
 
 const EmailVerifyPage = () => {
     useProtectedPath();
     const countRef = useRef<number>(-1);
+    const [countdownNumber, setCountdownNumber] = useState<number>(-1);
     const navigate = useNavigate();
     const [messageApi, contextHolder] = message.useMessage();
 
     const sendVerifyEmail = useCallback(() => {
+        if (countRef.current > 0) {
+            return;
+        }
         if (FirebaseManager.auth.currentUser && !FirebaseManager.auth.currentUser.emailVerified) {
-            // TODO: send email
             FirebaseManager.sendVerifyEmail()
                 .then(() => {
-                    countRef.current = 60;
+                    const totalCountdown = 60;
+                    countRef.current = totalCountdown;
+                    setCountdownNumber(totalCountdown);
                     messageApi.open({
                         type: "success",
                         content: "Email sent, please check your mail",
@@ -29,7 +36,7 @@ const EmailVerifyPage = () => {
                     });
                 });
         }
-    }, [messageApi]);
+    }, [messageApi, countRef]);
 
     const checkVerifyStateAndGoDashboard = useCallback(async () => {
         await FirebaseManager.auth.currentUser?.reload();
@@ -37,8 +44,13 @@ const EmailVerifyPage = () => {
             navigate("/dashboard", {
                 replace: true,
             });
+        } else {
+            messageApi.open({
+                type: "error",
+                content: "Email not verified",
+            });
         }
-    }, [navigate]);
+    }, [navigate, messageApi]);
 
     const Logout = useCallback(async () => {
         await FirebaseManager.auth.signOut();
@@ -55,31 +67,124 @@ const EmailVerifyPage = () => {
         const intervalId = setInterval(() => {
             if (countRef.current > 0) {
                 countRef.current--;
+                setCountdownNumber(countRef.current);
             }
         }, 1000);
         return () => {
             clearInterval(intervalId);
         };
-    }, [countRef]);
+    }, [countRef, setCountdownNumber]);
 
     return (
         <StyledContainer>
             {contextHolder}
-            <div>Please click active link in your email ({FirebaseManager.auth.currentUser?.email})</div>
-            {countRef.current > 0 ? (
-                <Button type="primary" disabled>
-                    {countRef.current}
+            <img src={OcelotLogo} alt="Ocelot Pay Logo" className="logo" />
+            <div className="title">Please verify your email</div>
+            <div className="desc">
+                We just sent an email to <br /> <span className="email">{FirebaseManager.auth.currentUser?.email}</span>
+                . <br /> Click the link in the email to verify your account.
+            </div>
+            <div className="spacer"></div>
+            <div className="go-console-block" onClick={checkVerifyStateAndGoDashboard}>
+                <span className="text">go console</span>
+                <div className="go-console-icon">
+                    <ArrowRightOutlined />
+                </div>
+            </div>
+            <div className="btn-row">
+                <Button
+                    type="primary"
+                    onClick={sendVerifyEmail}
+                    size="large"
+                    disabled={countdownNumber > 0 ? true : false}
+                    className="action-btn"
+                >
+                    {countdownNumber > 0 ? countdownNumber : "Resend email"}
                 </Button>
-            ) : (
-                <Button type="primary" onClick={sendVerifyEmail}>
-                    Send Verify Email
+                <Button onClick={Logout} size="large" className="action-btn">
+                    Change account
                 </Button>
-            )}
-            <Button onClick={checkVerifyStateAndGoDashboard}>Email verified</Button>
-            <Button onClick={Logout}>Logout</Button>
+            </div>
         </StyledContainer>
     );
 };
 
-const StyledContainer = styled.div.attrs({ className: "email-verify-page" })``;
+const StyledContainer = styled.div.attrs({ className: "email-verify-page" })`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .logo {
+        width: 80px;
+        height: 80px;
+        margin-bottom: 20px;
+    }
+    .title {
+        font-size: 32px;
+    }
+    .desc {
+        font-size: 16px;
+        margin-top: 20px;
+        text-align: center;
+        color: rgba(0, 0, 0, 0.6);
+        line-height: 24px;
+
+        .email {
+            font-weight: bold;
+            font-style: italic;
+        }
+    }
+    /* .spacer {
+        height: 100px;
+    } */
+    @keyframes moveArrow {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(20%);
+        }
+    }
+    .go-console-block {
+        font-size: 16px;
+        color: #1677ff;
+        cursor: pointer;
+        margin-right: 8px;
+        margin: 0px 0 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+            .go-console-icon {
+                animation-play-state: paused;
+            }
+        }
+
+        .go-console-icon {
+            padding: 8px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            animation-name: moveArrow;
+            animation-duration: 1s;
+            animation-iteration-count: infinite;
+            animation-direction: alternate;
+            animation-timing-function: ease-in-out;
+        }
+    }
+
+    .btn-row {
+        width: 350px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .action-btn {
+            width: 45%;
+        }
+    }
+`;
 export default EmailVerifyPage;
